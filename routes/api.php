fcbf5bb17d7413ad5d31091f49a33c761e7fc801
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-01-31 15:18:34 +0530

    update or user, logout and login access

--- routes/api.php
+++ routes/api.php
@@ -20,30 +20,42 @@ Route::post('/login/{otp?}', [AuthController::class, 'login']);
 
 Route::post('/get_otp', [AuthController::class, 'generate_otp']);
 
-Route::prefix('industry')->group(function () {
-    Route::post('/', [IndustryController::class, 'createIndustry']); // Create a new industry record
-    Route::get('/{id?}', [IndustryController::class, 'getIndustries']); // Retrieve industry record (all or specific)
-    Route::post('/{id}', [IndustryController::class, 'updateIndustry']); // Update a specific industry record
-    Route::delete('/{id}', [IndustryController::class, 'deleteIndustry']); // delete a specific industry record
-});
+Route::middleware(['auth:sanctum'])->group(function () {
 
-Route::prefix('sub_industry')->group(function () {
-    Route::post('/', [ReviewController::class, 'createSubIndustry']); // Create a new review record
-    Route::get('/{id?}', [ReviewController::class, 'getSubIndustries']); // Retrieve review record (all or specific)
-    Route::post('/{id}', [ReviewController::class, 'updateSubIndustry']); // Update a specific review record
-    Route::delete('/{id}', [ReviewController::class, 'deleteSubIndustry']); // Delete a specific review record
-});
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::prefix('product')->group(function () {
-    Route::post('/', [ProductController::class, 'createProduct']); // Create a new product
-    Route::get('/{id?}', [ProductController::class, 'fetchProducts']); // Retrieve product (all or specific)
-    Route::post('/{id}', [ProductController::class, 'updateProduct']); // Update a specific product
-    Route::post('/images/{id}', [ProductController::class, 'updateProductImages']); // Upload image for a specific product
-    Route::delete('/{id}', [ProductController::class, 'deleteProduct']); // Delete a specific product
-});
+    Route::prefix('industry')->group(function () {
+        Route::post('/', [IndustryController::class, 'createIndustry']); // Create a new industry record
+        Route::get('/{id?}', [IndustryController::class, 'getIndustries']); // Retrieve industry record (all or specific)
+        Route::post('/{id}', [IndustryController::class, 'updateIndustry']); // Update a specific industry record
+        Route::delete('/{id}', [IndustryController::class, 'deleteIndustry']); // delete a specific industry record
+    });
 
-Route::prefix('review')->group(function () {
-    Route::post('/', [ReviewController::class, 'createReview']); // Create a new review
-    Route::get('/{id?}', [ReviewController::class, 'getReviews']); // Retrieve review (all or specific)
-    Route::post('/{id}', [ReviewController::class, 'updateReview']); // Update a specific review
+    Route::prefix('sub_industry')->group(function () {
+        Route::post('/', [ReviewController::class, 'createSubIndustry']); // Create a new review record
+        Route::get('/{id?}', [ReviewController::class, 'getSubIndustries']); // Retrieve review record (all or specific)
+        Route::post('/{id}', [ReviewController::class, 'updateSubIndustry']); // Update a specific review record
+        Route::delete('/{id}', [ReviewController::class, 'deleteSubIndustry']); // Delete a specific review record
+    });
+
+    Route::prefix('product')->group(function () {
+        Route::post('/', [ProductController::class, 'createProduct']); // Create a new product
+        Route::get('/{id?}', [ProductController::class, 'fetchProducts']); // Retrieve product (all or specific)
+        Route::post('/{id}', [ProductController::class, 'updateProduct']); // Update a specific product
+        Route::post('/images/{id}', [ProductController::class, 'updateProductImages']); // Upload image for a specific product
+        Route::delete('/{id}', [ProductController::class, 'deleteProduct']); // Delete a specific product
+    });
+
+    Route::prefix('review')->group(function () {
+        Route::post('/', [ReviewController::class, 'createReview']); // Create a new review
+        Route::get('/{id?}', [ReviewController::class, 'getReviews']); // Retrieve review (all or specific)
+        Route::post('/{id}', [ReviewController::class, 'updateReview']); // Update a specific review
+    });
+
+    Route::prefix('user')->group(function () {
+        Route::post('/', [ReviewController::class, 'register']); // Create a new user
+        Route::get('/{id?}', [ReviewController::class, 'viewUsers']); // Retrieve user (all or specific)
+        Route::post('/{id}', [ReviewController::class, 'updateUser']); // Update a speciic user
+        Route::delete('/{id}', [ReviewController::class, 'deleteUser']); // Update a specific user
+    });
 });
