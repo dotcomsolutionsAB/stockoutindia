9b21076d45ea352ae4ce7ce99aad2429c6be633d
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-03-25 07:46:17 +0530

    ..

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    @include('partials.navbar') <!-- Optional: Include your navbar -->
-
     <main>
         @yield('content')
     </main>
