e98f471de4ff4b7ffd051259ff63d9d63cc000fb
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-03-31 12:35:51 +0530

    fix

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Hash;
+use App\Mail\SendNewPassword;
 use App\Models\User;
 use Auth;
-use Hash;
 
 class AuthController extends Controller
 {
