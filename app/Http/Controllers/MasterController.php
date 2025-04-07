5b69c5741366ed2acaa7948a8e45aab9a2aa3ae1
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-04-07 19:40:18 +0530

    ..

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -35,11 +35,7 @@ class MasterController extends Controller
     public function fetchAllStates()
     {
         try {
-            $states = StateModel::with('get_country:id,name')
-            ->whereHas('get_country', function ($query) {
-                $query->where('id', 101);
-            })
-            ->get();
+            
 
             // Transform response to replace nested "country" with "country_name"
             $states->transform(function ($state) {
