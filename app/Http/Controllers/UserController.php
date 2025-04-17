4476efa533bcfe1ad0dc65f03b507881dff430f9
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-04-17 16:19:06 +0530

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\RazorpayOrdersModel;
+use App\Models\Users;
+use App\Models\ProductModel;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
