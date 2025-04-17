e3bd65dd42c1f28b5f9396054abae7e0ed78b5e9
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-04-17 14:40:29 +0530

    fix col names

--- app/Models/RazorpayOrdersModel.php
+++ app/Models/RazorpayOrdersModel.php
@@ -28,12 +28,12 @@ class RazorpayOrdersModel extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user');
     }
 
     public function product()
     {
-        return $this->belongsTo(ProductModel::class, 'product_id');
+        return $this->belongsTo(ProductModel::class, 'product');
     }
 
 }
