cdf5be0cfc9d78f0e6d8a29c860cbd7d28f350d4
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-04-23 12:26:48 +0530

    update

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use App\Services\GoogleAuthService;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
